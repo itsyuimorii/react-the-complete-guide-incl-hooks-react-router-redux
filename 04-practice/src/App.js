b0898d7238ef4589b9816ef17a35537c18fb69d3
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-07-11 22:14:29 -0500

    practice 4

--- 04-practice/src/App.js
+++ 04-practice/src/App.js
@@ -1,88 +1,101 @@
- 
+
 import React from 'react';
- 
- 
+
+
 
 // eslint-disable-next-line
 const concepts = [
   {
     title: 'Standard',
     price: '$10/per month',
- 
+
     description:
       'Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.',
-    button_text:'Try for free',
+    button_text: 'Try for free',
     details: '90 days free trail'
-    },
+  },
   {
     title: 'Pro',
     price: '$20/per month',
- 
+
     description:
       'State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state and multiple components can share state.',
-      details: '90 days free trail'
-    },
+    details: '90 days free trail'
+  },
   {
     title: 'Business',
     price: '$30/per month',
- 
+
     description:
-      'Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event.',
-      details: '90 days free trail'
-    },
+      'Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event,components can share state.',
+    details: '90 days free trail'
+  },
 ];
 
 function App() {
   return (
     <div>
       <header>
-         <h1>A Plan for every business`s needs</h1>
-        <p>Choose the plan that works best for you and your team.</p>
+        <h1>Experience the Power of DataConnect Pro </h1>
+        <p>The Ultimate Job Search App for Data-Driven Professionals</p>
       </header>
       <ul id="concepts">
         <li className="concept">
           <h2>{concepts[0].title} </h2>
-          <p>{concepts[0].price} </p>
+          <h3>{concepts[1].price} </h3>
           <p>{concepts[0].description}</p>
           {/* <img src={concepts[0].image} alt={concepts[0].title} /> */}
-          <button type="button">{concepts[0].button_text}</button>
-          <p>{concepts[0].details}</p>
-          <ul>
+          <div className="button-container">
+            <button type="button">{concepts[0].button_text}</button>
+           
+          </div>
+          <p>30 day free trail</p>
+           <ul className ="details">
             <li>50 payments per month for free</li>
-            <li>3 files</li>
-            <li>30-day revision history</li>
+             <li>30-day revision history</li>
+            <li>50 payments per month for free</li>
+
           </ul>
+      
         </li>
- 
+        
+
         <li className="concept">
           <h2>{concepts[1].title} </h2>
-          <p>{concepts[1].price} </p>
+         
+          <h3>{concepts[1].price} </h3>
+     
           <p>{concepts[1].description}</p>
           {/* <img src={concepts[0].image} alt={concepts[0].title} /> */}
-          <button type="button">{concepts[0].button_text}</button>
-          <p>{concepts[1].details}</p>
-          <ul>
+          <div className="button-container">
+            <button type="button">{concepts[0].button_text}</button>
+          </div>
+          <p>30 day free trail</p>
+           <ul className ="details">
             <li>50 payments per month for free</li>
-            <li>3 files</li>
-            <li>30-day revision history</li>
+             <li>30-day revision history</li>
+            <li>50 payments per month for free</li>
+
           </ul>
         </li>
- 
+
         <li className="concept">
           <h2>{concepts[2].title} </h2>
-          <p>{concepts[2].price} </p>
+          <h3>{concepts[1].price} </h3>
           <p>{concepts[2].description}</p>
           {/* <img src={concepts[0].image} alt={concepts[0].title} /> */}
-          <button type="button">{concepts[0].button_text}</button>
-          <p>{concepts[2].details}</p>
-          <ul>
+          <div className="button-container">
+            <button type="button">{concepts[0].button_text}</button>
+          </div>
+          <p>30 day free trail</p>
+           <ul className ="details">
             <li>50 payments per month for free</li>
-            <li>3 files</li>
-            <li>30-day revision history</li>
+             <li>30-day revision history</li>
+            <li>50 payments per month for free</li>
           </ul>
         </li>
       </ul>
-      
+
     </div>
   );
 }
